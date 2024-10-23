023f7f23cd368b2478ed47f442b5d8183e375e5e
Gouten95 <gouten95@hotmail.com>
2024-10-22 18:00:54 -0700

    hover categorias home

--- app/components/Categorias.tsx
+++ app/components/Categorias.tsx
@@ -1,13 +1,12 @@
-"use client";
 import { Image, HStack, Box, Link } from "@chakra-ui/react";
 
 export default function Header() {
   return (
     <HStack padding={5} gap={{base: "5", sm: "5", md: "20"}} marginBottom={5}>
-      <Box maxWidth={{base: "100%", sm: "100%", md: "190px", lg: "250px"}} >
+      <Box maxWidth={{base: "100%", sm: "100%", md: "190px", lg: "250px"}} _hover={{transform: "scale(1.05)", boxShadow: "xl", transition: "transform 0.3s ease, box-shadow 0.3 ease"}} >
         <Link href="/productos/earcuffs-ely"><Image src="/img/image-earcuff-ely.jpg" alt="Imagen de la categoría de earcuff ely." /></Link>
       </Box>
-      <Box maxWidth={{base: "100%", sm: "100%", md: "190px", lg: "250px"}}>
+      <Box maxWidth={{base: "100%", sm: "100%", md: "190px", lg: "250px"}} _hover={{transform: "scale(1.05)", boxShadow: "xl", transition: "transform 0.3s ease, box-shadow 0.3 ease"}}>
         <Link href="/productos/aretes"><Image src="/img/imagen-aretes.jpg" alt="Imagen de la categoría de aretes." /></Link>
       </Box>
     </HStack>
